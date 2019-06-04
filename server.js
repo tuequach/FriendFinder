58e6821f12ac8cf5040f4f7e4bb7f7e98ffc0460
tuequach <tuequach@gmail.com>
2019-06-04 16:25:37 -0500

    adding in a port and seeting up all routings

--- server.js
+++ server.js
@@ -1,2 +1,22 @@
 var express = require('express');
 var path = require('path');
+var fs = require('fs');
+
+var app = express ();
+var PORT = process.envPORT || 3000;
+
+app.use(express.static('app/public'));
+
+
+app.use(bodyParser.urlencoded({ extended: false })); 
+app.use(bodyParser.json());
+
+//Routes
+require('./app/routing/apiRoutes.js')(app, path);
+require('./app/routing/htmlRoutes.js')(app, path);
+
+app.listen(PORT, function() {
+    console.log("Listening on this port" + PORT);
+
+})
+
