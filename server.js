3a4719165fc5ab83e130263de60ce425ca32c0b3
tuequach <tuequach@gmail.com>
2019-06-06 11:36:14 -0500

    adding in middleware bodyparser to parse json and changes to static file for css

--- server.js
+++ server.js
@@ -2,14 +2,21 @@ var express = require('express');
 var path = require('path');
 var fs = require('fs');
 
+//Middleware to parse json
+var bodyParser = require('body-parser');
+
 var app = express ();
 var PORT = process.envPORT || 3000;
 
+//static files if needed.
 app.use(express.static('app/public'));
+app.use(express.static(_dirname + "/app/css"));
 
 
-app.use(bodyParser.urlencoded({ extended: false })); 
+app.use(bodyParser.urlencoded({ extended: true })); 
 app.use(bodyParser.json());
+app.use(bodyParser.text());
+app.use(bodyParser.json({ type: 'application/vnd.api+json'}));
 
 //Routes
 require('./app/routing/apiRoutes.js')(app, path);
