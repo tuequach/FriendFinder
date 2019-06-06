fdc5c1f28ab6347b6b98adc532d7219d7d898734
tuequach <tuequach@gmail.com>
2019-06-06 11:57:42 -0500

    creating default of first friend as a match however, setting up the minimum calculation and the difference will result in a match. Also adding user to friend array after calculation

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -9,14 +9,32 @@ module.exports = function(app) {
     app.post('/api/friends', function(req, res){
         console.log(req.body.scores);
 
-        //details of user as a whole  and parsing  scores
+        //details of user as a whole and parsing  scores
         var user = req.body;
     for (var i =0; i< user.scores.length; i++) {
         user.scores[i] = parseInt(user.scores[i]);
     }
 
+    //default of first friend as a match but display results of whoever has the minimum difference in scores
+    var bestFriendIndex = 0;
+    var minimumDifference = 30;
 
-  
+        for (var i =0; i < friends.length; i++){
+            var totalDifference = 0; 
+            for (var j=0; j <friends[i].scores.length; j++) {
+                var difference = Math.abs(user.scores[j] - friends[i].scores[j]);
+                totalDifference += difference;
+                    
+            }
+            //if new mininmum, change index and set new minimum for comparison
+        if(totalDifference < minimumDifference){ 
+            bestFriendIndex = i;
+            minimumDifference = totalDifference; 
+            }
+        }
+        
+        //adding user to friend array
+        friends.push(user);
 
     })
 
