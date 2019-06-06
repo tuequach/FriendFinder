075284878bdbf4770baacc1bafbc42276e221440
tuequach <tuequach@gmail.com>
2019-06-06 11:42:50 -0500

    fixed syntax errors

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -9,4 +9,4 @@ module.exports = function (app, path)  {
         app.get('/survey', function (req,res){
             res.sendFile(path.join(__dirname, '/../public/survey.html'));
         });
-}
+};
