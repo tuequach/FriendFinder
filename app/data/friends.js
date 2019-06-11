9f7daa78fa16bda456e854986a9ca51d246b5e67
tuequach <tuequach@gmail.com>
2019-06-11 18:44:37 -0500

    fixed module friends export

--- app/data/friends.js
+++ app/data/friends.js
@@ -8,18 +8,23 @@ var friends = [
         "name":"Todoraki",
         "photo":"https://cdn.myanimelist.net/images/characters/3/299595.jpg",
         "scores":[2, 5, 4, 1, 2, 3, 3, 2, 3, 3]
-    },  {
+    },  
+    {
         "name":"Bakugo",
         "photo":"https://media.licdn.com/mpr/mpr//p/6/005/064/1bd/3435aa3.jpg",
         "scores":[1, 1, 5, 3, 3, 5, 1, 2, 3, 4]
-    },  {
+    },  
+    {
         "name":"Lida",
         "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
         "scores":[3, 1, 3, 5, 2, 3, 2, 2, 5, 5]
-    },  {
+    },  
+    {
         "name":"Uraraka",
         "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
         "scores":[3, 1, 4, 4, 2, 4, 2, 5, 1, 3]
-    },
+    }
 
-]
+];
+
+module.exports = friends;
--- server.js
+++ server.js
@@ -5,14 +5,13 @@ var path = require('path');
 var bodyParser = require('body-parser');
 
 var app = express ();
-var PORT = process.envPORT || 8080;
+var PORT = process.env.PORT || 8080;
 
 //static files if needed.
 app.use(express.static(__dirname + "/app/css"));
 
-
-app.use(bodyParser.urlencoded({ extended: true })); 
 app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true })); 
 app.use(bodyParser.text());
 app.use(bodyParser.json({ type: 'application/vnd.api+json'}));
 
